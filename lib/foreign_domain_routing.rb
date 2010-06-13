c06950f6f6420f6578a67a9e4dd4da521c26690d
Jorge Yau <jorgeyau@gmail.com>
2010-06-12 19:16:32 -0500

    Added a configurable option to treat subdomains as foreign domains too

--- lib/foreign_domain_routing.rb
+++ lib/foreign_domain_routing.rb
@@ -2,8 +2,10 @@ require 'foreign_domain_routing/routing_extensions'
 
 module ForeignDomainRouting
   DEFAULT_NATIVE_DOMAINS = {:development => ['localhost:3000'], :test => ['test.host'], :production => ['example.com'] }
-  mattr_accessor :init_native_domains
+  DEFAULT_FOREIGN_SUBDOMAINS = {:development => false, :test => false, :production => false}
+  mattr_accessor :init_native_domains, :init_foreign_subdomains
   @@init_native_domains = DEFAULT_NATIVE_DOMAINS.dup
+  @@init_foreign_subdomains = DEFAULT_FOREIGN_SUBDOMAINS.dup
     
   def self.native_domains
     init_native_domains[RAILS_ENV.to_sym]
@@ -13,9 +15,21 @@ module ForeignDomainRouting
     init_native_domains[RAILS_ENV.to_sym] = value
   end
 
+  def self.foreign_subdomains
+    init_foreign_subdomains[RAILS_ENV.to_sym]
+  end
+
+  def self.foreign_subdomains=(value)
+    init_foreign_subdomains[RAILS_ENV.to_sym] = value
+  end
+
   def self.foreign_domain?(host)
     native_domains.each do |domain|
-      return false if host =~ /#{domain}\Z/i
+      if foreign_subdomains
+        return false if host == domain # subdomains are foreign too.
+      else
+        return false if host =~ /#{domain}\Z/i
+      end
     end
     true
   end
